c46a39c6b535fa6cd955f259f3bf752fc2bfce13
Jan Pfenning <jan.pfenning@gmail.com>
2024-12-05 12:28:28 +0100

    add parsing utils for lists with seperator

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,3 +1,11 @@
 export const asCharMatrix = (rawInput: string): string[][] => {
-    return rawInput.split(/\n/g).map((line) => line.split(''));
+    return asManyCharSeperatedLists('', rawInput);
+};
+
+export const asManyCharSeperatedLists = (seperator: string, rawInput: string) => {
+    return rawInput.split(/\n/g).map((line) => line.split(seperator));
+};
+
+export const asManyCharSeperatedNumberLists = (seperator: string, rawInput: string): number[][] => {
+    return rawInput.split(/\n/g).map((line) => line.split(seperator).map((val) => +val));
 };
