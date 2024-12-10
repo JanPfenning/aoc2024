26191b2b6fd86dac86fde27afeae6c3b8aa52233
Jan Pfenning <jan.pfenning@gmail.com>
2024-12-10 13:49:43 +0100

    add parse method for digit matrices

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -2,6 +2,10 @@ export const asCharMatrix = (rawInput: string): string[][] => {
     return asManyCharSeperatedLists('', rawInput);
 };
 
+export const asDigitMatrix = (rawInput: string): number[][] => {
+    return asManyCharSeperatedNumberLists('', rawInput);
+};
+
 export const asManyCharSeperatedLists = (seperator: string, rawInput: string) => {
     return rawInput.split(/\n/g).map((line) => line.split(seperator));
 };
