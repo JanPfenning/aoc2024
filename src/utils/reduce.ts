f99594439e269a1c0dabe11766e899b4477651f1
Jan Pfenning <jan.pfenning@gmail.com>
2024-12-13 12:15:58 +0100

    add reducer to split an array into an array of batches of size n

--- src/utils/reduce.ts
+++ src/utils/reduce.ts
@@ -1,3 +1,14 @@
 export const sum = (acc: number, iter: number): number => {
     return acc + iter;
 };
+
+export const batch = <T>(batchSize: number) => {
+    return (batches: T[][], item: T, index: number): T[][] => {
+        const batchIndex = Math.floor(index / batchSize);
+        if (!batches[batchIndex]) {
+            batches[batchIndex] = [];
+        }
+        batches[batchIndex].push(item);
+        return batches;
+    };
+};
