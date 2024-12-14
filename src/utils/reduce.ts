eeb8f1735ba3b42a9d9c1ecc70bdeeca54c97dbe
Jan Pfenning <jan.pfenning@gmail.com>
2024-12-14 15:27:08 +0100

    add utils to reduce an array to its groups and counts

--- src/utils/reduce.ts
+++ src/utils/reduce.ts
@@ -6,6 +6,16 @@ export const product = (acc: number, iter: number): number => {
     return acc * iter;
 };
 
+export const complexGroup = <T>(equalityFn: (a: T, b: T) => boolean) => {
+    return (acc: Record<string, number>, curr: T): Record<string, number> => {
+        const key = Object.keys(acc).find((k) => equalityFn(JSON.parse(k), curr)) || JSON.stringify(curr);
+        acc[key] = (acc[key] || 0) + 1;
+        return acc;
+    };
+};
+
+export const group = complexGroup((a, b) => a === b);
+
 export const batch = <T>(batchSize: number) => {
     return (batches: T[][], item: T, index: number): T[][] => {
         const batchIndex = Math.floor(index / batchSize);
