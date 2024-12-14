3eec580a199b1862050f2f78bdc72b96c20b509e
Jan Pfenning <jan.pfenning@gmail.com>
2024-12-14 15:26:42 +0100

    add product reducer for repeated multiplication

--- src/utils/reduce.ts
+++ src/utils/reduce.ts
@@ -2,6 +2,10 @@ export const sum = (acc: number, iter: number): number => {
     return acc + iter;
 };
 
+export const product = (acc: number, iter: number): number => {
+    return acc * iter;
+};
+
 export const batch = <T>(batchSize: number) => {
     return (batches: T[][], item: T, index: number): T[][] => {
         const batchIndex = Math.floor(index / batchSize);
